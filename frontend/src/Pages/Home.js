7405322637923935596c37920e5dc32f5b4c0d56
Samit-prsl <samit.prsl@gmail.com>
2023-07-15 02:04:17 +0530

    frontend_2nd

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -3,12 +3,12 @@ import React from 'react'
 import SideBar from '../Components/SideBar'
 export default function Home() {
   return (
-    <div className=' font-[Open sans] bg-gradient-to-t from-[#000000] to-[#3D0000] h-screen '>
+    <div className=' font-[Open sans] bg-gradient-to-t from-[#000000] to-[#3D0000]  '>
    <div className=' relative z-10'>
    <SideBar/>
    </div>
-   <div className=' flex flex-col justify-center items-center '>
-   <div className=' text-xl text-center font-[Bebas Neue] pt-16 text-red-400 font-bold tracking-[0.9rem] outline-8 '>GymUpp</div>
+   <div className=' flex flex-col justify-center items-center pb-28 '>
+   <div className=' text-xl text-center font-[Bebas Neue] pt-36 text-red-400 font-bold tracking-[0.9rem] outline-8 '>GymUpp</div>
     <div className="text-5xl lg:text-7xl text-center text-gray-500 pt-16">Get Your Best</div>
     <div className="text-7xl lg:text-9xl text-center text-white pt-3 pl-2">Body Shape.</div>
    <div className=' pt-10 flex justify-center'>
